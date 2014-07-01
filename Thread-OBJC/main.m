787098db1f4cc36c68bc791c6dcc8d98e3fc0cb4
mar Jinn <marjinn@mars-Mac.local>
2014-07-02 04:30:33 +0530

    Initial Commit

---
+++ Thread-OBJC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Thread-OBJC
+//
+//  Created by mar Jinn on 7/2/14.
+//  Copyright (c) 2014 mar Jinn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
